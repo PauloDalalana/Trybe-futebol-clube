77828de810f62cd6391fd0c4c3448d0bfbd07c9b
PauloDalalana Turma37 <pauloterresdalalana@gmail.com>
2024-08-05 23:33:09 -0300

    corrigindo erros de linter

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -4,7 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import { Team } from '../database/models/Team.Model';
+import Team from '../database/models/Team.Model';
 
 import { Response } from 'superagent';
 
